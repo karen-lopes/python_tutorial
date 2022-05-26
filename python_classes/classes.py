283fdc2a1e59938c64f85718da90b9b8c48d82e4
Karen Lopes <karen.lopes@outlook.com>
2022-05-26 08:24:54 -0300

    classes and file reader

--- python_classes/classes.py
+++ python_classes/classes.py
@@ -1,3 +1,6 @@
+from operator import mod
+
+
 class Dog:
     """A simple attempt to model a dog."""
 
@@ -150,3 +153,45 @@ my_used_car.read_odometer()
 
 my_used_car.increment_odometer(100)
 my_used_car.read_odometer()
+
+
+# __init__() Method from the parent class
+
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odormeter_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class EletricCar(Car):
+    """Represent aspects of a car, specific to eletric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+
+my_tesla = EletricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+
+
+#super() function allows you to call a method from the parent class.
+
